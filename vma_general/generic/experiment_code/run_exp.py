cf979da9a36bb4e200ed5a7d51e47eb9e45ebb78
crossley <matthewjohncrossley@gmail.com>
2023-07-25 18:42:40 +1000

    liberty add

--- vma_general/generic/experiment_code/run_exp.py
+++ vma_general/generic/experiment_code/run_exp.py
@@ -14,6 +14,99 @@ import numpy as np
 import pandas as pd
 
 sub_num = 0
+use_liberty = False
+
+
+# This method grabs the position of the sensor
+def getPosition(ser, recordsize, averager):
+    ser.reset_input_buffer()
+
+    # Set variables
+    # This defines the length of the binary header (bytes 0-7)
+    header = 8
+    # This defines the bytesize of IEEE floating point
+    byte_size = 4
+
+    # Obtain data
+    ser.write(b'P')
+    # time.sleep(0.1)
+    # print("inWaiting " + str(ser.inWaiting()))
+    # print("recorded size " + str(recordsize))
+
+    # Read header to remove it from the input buffer
+    ser.read(header)
+
+    positions = []
+
+    # Read the three coordinates
+    for x in range(3):
+        # Read the coordinate
+        coord = ser.read(byte_size)
+
+        # Convert hex to floating point (little endian order)
+        coord = struct.unpack('<f', coord)[0]
+
+        positions.append(coord)
+
+    return positions
+
+
+if use_liberty:
+
+    ser = serial.Serial()
+    ser.baudrate = 115200
+    ser.port = 'COM1'
+
+    print(ser)
+    ser.open()
+
+    # Checks serial port if open
+    if (ser.is_open == False):
+        print("Error! Serial port is not open")
+        exit()
+
+    # Send command to receive data through port
+    ser.write(b'P')
+    time.sleep(1)
+
+    # Checks if Liberty is responding(e.g on)
+    if (ser.inWaiting() < 1):
+        print("Error! Check if liberty is on!")
+        exit()
+
+    # Set liberty output mode to binary
+    ser.write(b'F1\r')
+    time.sleep(1)
+
+    # Set distance unit to centimeters
+    ser.write(b'U1\r')
+    time.sleep(0.1)
+
+    # Set hemisphere to +Z
+    ser.write(b'H1,0,0,1\r')
+    time.sleep(0.1)
+
+    # Set sample rate to 240hz
+    ser.write(b'R4\r')
+    time.sleep(0.1)
+
+    # Reset frame count
+    ser.write(b'Q1\r')
+    time.sleep(0.1)
+
+    # Set output to only include position (no orientation)
+    ser.write(b'O1,3,9\r')
+    time.sleep(0.1)
+    ser.reset_input_buffer()
+
+    # Obtain data
+    ser.write(b'P')
+    time.sleep(0.1)
+
+    # Size of response
+    recordsize = ser.inWaiting()
+    ser.reset_input_buffer()
+    averager = 4
 
 win = visual.Window(size=(700, 700),
                     pos=(100, 100),
@@ -102,7 +195,13 @@ while current_trial < num_trials:
     resp = event.getKeys(keyList=['escape'])
     rt = state_clock.getTime()
 
-    x, y = mouse.getPos()
+    if use_liberty:
+        c_position = getPosition(ser, recordsize, averager)
+        x = c_position[0]
+        y = c_position[1]
+    else:
+        x, y = mouse.getPos()
+
     theta, r = coordinatetools.cart2pol(x, y)
 
     cursor_circle.pos = (x, y)
