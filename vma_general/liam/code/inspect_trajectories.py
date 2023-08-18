06bd998032999e8356adfa066c3184bab951e36e
crossley <matthewjohncrossley@gmail.com>
2023-08-18 18:53:39 +1000

    conflict

--- vma_general/liam/code/inspect_trajectories.py
+++ vma_general/liam/code/inspect_trajectories.py
@@ -4,76 +4,63 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from scipy.interpolate import CubicSpline
 
-d_config = pd.read_csv("../config/config_reach_1.csv")
-d_config = d_config[
-    [
-        "trial",
-        "no_uncertainty",
-        "low_uncertainty",
-        "high_uncertainty",
-        "unlimited_uncertainty",
-    ]
-]
+d_config = pd.read_csv("../config/config_reach_0.csv")
+d_move = pd.read_csv(
+    "../data/data_movements_0.csv",
+    index_col=0
+)
 
-d_move = pd.read_csv("../data/data_movements_1.csv")
+d_config = d_config.sort_values(["trial"])
+d_move = d_move.sort_values(["sample", "time", "trial"])
+d_move = d_move[d_move["state"] == "reach"]
+
 
 d = pd.merge(d_move, d_config, on="trial")
 d = d.sort_values(["sample", "time", "trial"])
 d = d[d["state"] == "reach"]
+d["target_angle"] = d["target_angle"].astype("category")
 
-d = d[
-    [
-        "sample",
-        "time",
-        "x",
-        "y",
-        "trial",
-        "no_uncertainty",
-        "low_uncertainty",
-        "high_uncertainty",
-        "unlimited_uncertainty",
-    ]
-]
+fig, ax = plt.subplots(2, 1, squeeze=False)
+sns.scatterplot(data=d, x="trial", y="rot", hue="phase", ax=ax[0, 0])
+sns.scatterplot(data=d, x="trial", y="target_angle", hue="phase", ax=ax[1, 0])
+plt.show()
 
-d["sig_mp"] = (
-    d["no_uncertainty"]
-    + d["low_uncertainty"]
-    + d["high_uncertainty"]
-    + d["unlimited_uncertainty"]
-)
+# inspect d_config
+d_config.plot(subplots=True)
+plt.show()
 
-d["sig_mp_cat"] = pd.from_dummies(
-    d[
-        [
-            "no_uncertainty",
-            "low_uncertainty",
-            "high_uncertainty",
-            "unlimited_uncertainty",
-        ]
-    ],
-    default_category="WHAT?",
-)
+# inspect d_move
+fig, ax = plt.subplots(2, 2, squeeze=False)
+sns.scatterplot(data=d_move, x="sample", y="time", hue="trial", ax=ax[0, 0])
+sns.scatterplot(data=d_move, x="time", y="x", hue="trial", ax=ax[0, 1])
+sns.scatterplot(data=d_move, x="time", y="y", hue="trial", ax=ax[1, 0])
+sns.scatterplot(data=d_move, x="x", y="y", hue="trial", ax=ax[1, 1])
+plt.show()
 
-fig, ax = plt.subplots(2, 3, squeeze=False)
+
+fig, ax = plt.subplots(3, 4, squeeze=False)
 ax = ax.flatten()
-for i, smp in enumerate(d["sig_mp_cat"].unique()):
+for i, ta in enumerate(d["target_angle"].unique()):
     sns.scatterplot(
-        data=d[d["sig_mp_cat"] == smp],
+        data=d[d["target_angle"] == ta],
         x="x",
         y="y",
         hue="trial",
+        legend=False,
         ax=ax[i],
     )
-    ax[i].set_title(smp)
+    ax[i].set_title(ta)
 plt.tight_layout()
 plt.show()
 
 
 def interpolate_movements(d):
-
     t = d["time"]
     x = d["x"]
     y = d["y"]
+
+    print(d)
+    print(t)
 
     xs = CubicSpline(t, x)
     ys = CubicSpline(t, y)
@@ -84,14 +71,14 @@ def interpolate_movements(d):
 
     relsamp = np.arange(0, tt.shape[0], 1)
 
-    # fig, ax = plt.subplots(2, 3, squeeze=False)
-    # ax[0, 0].scatter(t, x, c=t)
-    # ax[0, 1].scatter(t, y, c=t)
-    # ax[0, 2].scatter(x, y, c=t)
-    # ax[1, 0].scatter(tt, xx, c=tt)
-    # ax[1, 1].scatter(tt, yy, c=tt)
-    # ax[1, 2].scatter(xx, yy, c=tt)
-    # plt.show()
+    fig, ax = plt.subplots(2, 3, squeeze=False)
+    ax[0, 0].scatter(t, x, c=t)
+    ax[0, 1].scatter(t, y, c=t)
+    ax[0, 2].scatter(x, y, c=t)
+    ax[1, 0].scatter(tt, xx, c=tt)
+    ax[1, 1].scatter(tt, yy, c=tt)
+    ax[1, 2].scatter(xx, yy, c=tt)
+    plt.show()
 
     dd = pd.DataFrame({"relsamp": relsamp, "time": tt, "x": xx, "y": yy})
 
@@ -99,10 +86,9 @@ def interpolate_movements(d):
 
 
 dd = (
-    d.groupby(["trial", "sig_mp_cat"])[["time", "x", "y"]]
-    .apply(interpolate_movements)
-    .droplevel(2)
-    .reset_index(drop=False)
+    d.groupby(["trial"])[["time", "x", "y"]].apply(interpolate_movements)
+    # .droplevel(2)
+    # .reset_index(drop=False)
 )
 
 # dd = dd.sort_values(["trial", "relsamp"])
