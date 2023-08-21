a771e306b325846c4693e5b55237f27ccbcb584f
crossley <matthewjohncrossley@gmail.com>
2023-08-21 13:49:48 +1000

    conflict with liam commit

--- vma_general/dhruva/code/inspect_trajectory.py
+++ vma_general/dhruva/code/inspect_trajectory.py
@@ -4,7 +4,9 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from scipy.interpolate import CubicSpline
 
+d_move = pd.read_csv("../data/data_movements_1.csv")
 d_config = pd.read_csv("../config/config_reach_1.csv")
+
 d_config = d_config[
     [
         "trial",
@@ -15,7 +17,17 @@ d_config = d_config[
     ]
 ]
 
-d_move = pd.read_csv("../data/data_movements_1.csv")
+d_config["phase"] = (
+    ["base"] * 20
+    + ["adapt_1"] * 45
+    + ["adapt_2"] * 45
+    + ["adapt_3"] * 45
+    + ["adapt_4"] * 45
+    + ["wash"] * 100
+)
+
+d_config.plot(subplots=True)
+plt.show()
 
 d = pd.merge(d_move, d_config, on="trial")
 d = d.sort_values(["sample", "time", "trial"])
@@ -28,6 +40,7 @@ d = d[
         "x",
         "y",
         "trial",
+        "phase",
         "no_uncertainty",
         "low_uncertainty",
         "high_uncertainty",
@@ -35,42 +48,38 @@ d = d[
     ]
 ]
 
+d = d[np.isin(d["phase"], ["adapt_1", "adapt_2", "adapt_3", "adapt_4"])]
+
 d["sig_mp"] = (
     d["no_uncertainty"]
-    + d["low_uncertainty"]
-    + d["high_uncertainty"]
-    + d["unlimited_uncertainty"]
-)
+    + 2 * d["low_uncertainty"]
+    + 3 * d["high_uncertainty"]
+    + 4 * d["unlimited_uncertainty"]
+).astype("category")
+d["sig_mp"] = d["sig_mp"].cat.rename_categories(["low", "med", "high", "Inf"])
 
-d["sig_mp_cat"] = pd.from_dummies(
-    d[
-        [
-            "no_uncertainty",
-            "low_uncertainty",
-            "high_uncertainty",
-            "unlimited_uncertainty",
-        ]
-    ],
-    default_category="WHAT?",
+fig, ax = plt.subplots(2, 2, squeeze=False)
+sns.scatterplot(
+    data=d[d["phase"] == "adapt_1"], x="x", y="y", hue="sig_mp", ax=ax[0, 0]
 )
-
-fig, ax = plt.subplots(2, 3, squeeze=False)
-ax = ax.flatten()
-for i, smp in enumerate(d["sig_mp_cat"].unique()):
-    sns.scatterplot(
-        data=d[d["sig_mp_cat"] == smp],
-        x="x",
-        y="y",
-        hue="trial",
-        ax=ax[i],
-    )
-    ax[i].set_title(smp)
+sns.scatterplot(
+    data=d[d["phase"] == "adapt_2"], x="x", y="y", hue="sig_mp", ax=ax[0, 1]
+)
+sns.scatterplot(
+    data=d[d["phase"] == "adapt_3"], x="x", y="y", hue="sig_mp", ax=ax[1, 0]
+)
+sns.scatterplot(
+    data=d[d["phase"] == "adapt_4"], x="x", y="y", hue="sig_mp", ax=ax[1, 1]
+)
+ax[0, 0].set_title("adapt 1")
+ax[0, 1].set_title("adapt 2")
+ax[1, 0].set_title("adapt 3")
+ax[1, 1].set_title("adapt 4")
 plt.tight_layout()
 plt.show()
 
 
 def interpolate_movements(d):
-
     t = d["time"]
     x = d["x"]
     y = d["y"]
@@ -84,62 +93,91 @@ def interpolate_movements(d):
 
     relsamp = np.arange(0, tt.shape[0], 1)
 
-    # fig, ax = plt.subplots(2, 3, squeeze=False)
-    # ax[0, 0].scatter(t, x, c=t)
-    # ax[0, 1].scatter(t, y, c=t)
-    # ax[0, 2].scatter(x, y, c=t)
-    # ax[1, 0].scatter(tt, xx, c=tt)
-    # ax[1, 1].scatter(tt, yy, c=tt)
-    # ax[1, 2].scatter(xx, yy, c=tt)
-    # plt.show()
-
     dd = pd.DataFrame({"relsamp": relsamp, "time": tt, "x": xx, "y": yy})
 
     return dd
 
 
 dd = (
-    d.groupby(["trial", "sig_mp_cat"])[["time", "x", "y"]]
+    d.groupby(["phase", "trial", "sig_mp"])[["time", "x", "y"]]
     .apply(interpolate_movements)
-    .droplevel(2)
+    .droplevel(-1)
     .reset_index(drop=False)
 )
 
-# dd = dd.sort_values(["trial", "relsamp"])
+dd = dd.sort_values(["trial", "relsamp"])
 
-# fig, ax = plt.subplots(2, 2, squeeze=False)
-# ax = ax.flatten()
-# for i, smp in enumerate(dd["sig_mp_cat"].unique()):
-#     sns.scatterplot(
-#         data=dd[dd["sig_mp_cat"] == smp],
-#         x="x",
-#         y="y",
-#         hue="trial",
-#         ax=ax[i],
-#     )
-#     ax[i].set_title(smp)
-# plt.tight_layout()
+fig, ax = plt.subplots(2, 2, squeeze=False)
+sns.scatterplot(
+    data=dd[dd["phase"] == "adapt_1"], x="x", y="y", hue="sig_mp", ax=ax[0, 0]
+)
+sns.scatterplot(
+    data=dd[dd["phase"] == "adapt_2"], x="x", y="y", hue="sig_mp", ax=ax[0, 1]
+)
+sns.scatterplot(
+    data=dd[dd["phase"] == "adapt_3"], x="x", y="y", hue="sig_mp", ax=ax[1, 0]
+)
+sns.scatterplot(
+    data=dd[dd["phase"] == "adapt_4"], x="x", y="y", hue="sig_mp", ax=ax[1, 1]
+)
+ax[0, 0].set_title("adapt 1")
+ax[0, 1].set_title("adapt 2")
+ax[1, 0].set_title("adapt 3")
+ax[1, 1].set_title("adapt 4")
+plt.tight_layout()
+plt.show()
+
+
+ddd = (
+    dd.groupby(["phase", "sig_mp", "relsamp"])[["time", "x", "y"]].mean().reset_index()
+)
+
+fig, ax = plt.subplots(2, 2, squeeze=False)
+sns.scatterplot(
+    data=ddd[ddd["phase"] == "adapt_1"], x="x", y="y", hue="sig_mp", ax=ax[0, 0]
+)
+sns.scatterplot(
+    data=ddd[ddd["phase"] == "adapt_2"], x="x", y="y", hue="sig_mp", ax=ax[0, 1]
+)
+sns.scatterplot(
+    data=ddd[ddd["phase"] == "adapt_3"], x="x", y="y", hue="sig_mp", ax=ax[1, 0]
+)
+sns.scatterplot(
+    data=ddd[ddd["phase"] == "adapt_4"], x="x", y="y", hue="sig_mp", ax=ax[1, 1]
+)
+ax[0, 0].set_title("adapt 1")
+ax[0, 1].set_title("adapt 2")
+ax[1, 0].set_title("adapt 3")
+ax[1, 1].set_title("adapt 4")
+plt.tight_layout()
+plt.show()
+
+
+# TODO: this should be velocity based
+def compute_imv(d):
+    t = d["time"].to_numpy()
+    x = d["x"].to_numpy()
+    y = d["y"].to_numpy()
+    t = t - t.min()
+    theta = np.arctan2(y, x) * (180 / np.pi)
+    imv = theta.mean()
+    print(imv)
+    return imv
+
+
+dd["imv"] = dd.groupby(["trial"]).apply(compute_imv)
+
+fig, ax = plt.subplots(1, 1, squeeze=False)
+sns.scatterplot(data=dd, x="trial", y="imv", hue="sig_mp", ax=ax[0, 0])
+plt.show()
+
+# vx = np.diff(ddd["x"]) / np.diff(ddd["time"])
+# vy = np.diff(ddd["y"]) / np.diff(ddd["time"])
+# vxy = np.sqrt(vx**2 + vy**2)
+# vt = ddd["time"].to_numpy()[:-1]
+
+# fig, ax = plt.subplots(3, 1, squeeze=False)
+# ax[0, 0].scatter(vt, vx)
+# ax[1, 0].scatter(vt, vy)
+# ax[2, 0].scatter(vt, vxy)
 # plt.show()
-
-
-# ddd = dd.groupby(["sig_mp_cat", "relsamp"])[["time", "x", "y"]].mean().reset_index()
-
-# fig, ax = plt.subplots(2, 2, squeeze=False)
-# ax = ax.flatten()
-# for i, smp in enumerate(dd["sig_mp_cat"].unique()):
-#     sns.scatterplot(
-#         data=ddd[ddd["sig_mp_cat"] == smp], x="x", y="y", hue="time", ax=ax[i]
-#     )
-#     ax[i].set_title(smp)
-# plt.tight_layout()
-# plt.show()
-
-
-# # vx = np.diff(ddd["x"]) / np.diff(ddd["time"])
-# # vy = np.diff(ddd["y"]) / np.diff(ddd["time"])
-# # vel = np.sqrt(vx**2 + vy**2)
-# # vt = ddd["time"].to_numpy()[:-1]
-
-# # fig, ax = plt.subplots(1, 2, squeeze=False)
-# # ax[0, 0].plot(vt, vel)
-# # plt.show()
