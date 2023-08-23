4a6e820eb64219ab15bf96403f51d40cf0dcd8ea
Dhruva Dave <dhruvadave@Dhruvas-MacBook-Pro.local>
2023-08-23 19:19:13 +1000

    Changed config files and experiment run code to fit method modifications. Cleaned up folders.

--- vma_general/dhruva/code/make_config_file_reach.py
+++ vma_general/dhruva/code/make_config_file_reach.py
@@ -123,55 +123,14 @@ d['cycle'] = cycle
 d['target_angle'] = target_angle
 d['target_angle'] = d.groupby(
     ['cycle'])['target_angle'].sample(frac=1).reset_index(drop=True)
-''' 
-d['instruct_phase'] = instruct_phase
-d['instruct_state'] = instruct_state 
 
-nn = [n_fam, n_base, n_clamp, n_adaptation, n_wash]
-nn = [x * n_targets for x in nn]
-labels = ['Familiarisation', 'Baseline', 'Clamp', 'Generalisation', 'Washout']
-labels_x = np.concatenate(([0], np.cumsum(nn)[:-1]))
-fig, ax = plt.subplots(1, 1, squeeze=False)
-ax[0, 0].scatter(trial, rot, c=target_angle)
-ax[0, 0].vlines(labels_x, 0, rot_amp + 5, 'k', '--')
-for i in range(len(labels)):
-    ax[0, 0].text(labels_x[i], np.max(rot) + 5, labels[i], rotation=30)
-ax[0, 0].set_ylabel('Rotation (degrees)')
-ax[0, 0].set_xlabel('Trial')
-plt.show()
-'''
-'''
-n_subs_per_cnd = 20
-conditions = ['explicit_instruct'] * n_subs_per_cnd
-np.random.shuffle(conditions)
-'''
+#lookup_df = pd.read_excel('../condition_assignment.xls')
 
-lookup_df = pd.read_excel('../condition_assignment.xls')
-
-for sub in range(41):
-    if sub == 0:
-        d.to_csv('../config/config_reach_' + str(sub) + '.csv', index=False)
-    else:
-        condition = lookup_df.iloc[sub - 1].experimentalCondition
-        d['experimental_condition'] = condition
+for sub in range(20):
+#     if sub == 0:
+#         d.to_csv('../config/config_reach_' + str(sub) + '.csv', index=False)
+#     else:
+#         condition = lookup_df.iloc[sub - 1].experimentalCondition
+#         d['experimental_condition'] = condition
         d.to_csv('../config/config_reach_' + str(sub) + '.csv', index=False)
 
-
-
-'''
-
-df
-
-a b
-1 2
-3 4
-5 6
-
-df['c'] = 'hello'
-
-a b c
-1 2 'hello'
-3 4 'hello'
-5 6 'hello'
-
-'''
