49ba239f40acfc8caf34d6cc7cb21e043cf802fd
crossley <matthewjohncrossley@gmail.com>
2023-08-30 14:27:57 +1000

    code for dhruvas project

--- vma_general/dhruva/code/inspect_trajectory.py
+++ vma_general/dhruva/code/inspect_trajectory.py
@@ -2,173 +2,56 @@ from imports import *
 from util_func import *
 
 d = load_all_data()
+d = d.sort_values(["condition", "subject", "trial"])
 
-# NOTE: grab a subset to play with
-d = d[d.subject == 1]
-d = d[d.trial < 10]
+d = d.groupby(["condition", "subject", "trial"], group_keys=False).apply(
+    compute_kinematics
+)
 
-def inspect_move(d):
-    t = d["time"].to_numpy()
-    x = d["x"].to_numpy()
-    y = d["y"].to_numpy()
+dd = d.groupby(
+    ["condition", "subject", "phase", "trial", "sig_mp"], group_keys=False
+).apply(interpolate_movements)
 
-    vx = np.gradient(x, t)
-    vy = np.gradient(y, t)
-    v = np.sqrt(vx**2 + vy**2)
-    d['v'] = v
+ddd = (
+    dd.groupby(["condition", "subject", "phase", "sig_mp", "relsamp"])[
+        ["time", "x", "y", "v"]
+    ]
+    .mean()
+    .reset_index()
+)
 
-    radius = np.sqrt(x**2 + y**2)
-    theta = (np.arctan2(y, x) + np.pi) * 180 / np.pi
-    imv = theta[v > 0.05 * v.max()][0]
-    emv = theta[-1]
-    d["imv"] = imv
-    d["emv"] = emv
-    # print(d.target_angle.unique()[0], imv, emv)
+print(d)
+print(dd)
+print(ddd)
 
-    xs = CubicSpline(t, x)
-    ys = CubicSpline(t, y)
-    vs = CubicSpline(t, v)
-
-    tt = np.linspace(t.min(), t.max(), 100)
-    xx = xs(tt)
-    yy = ys(tt)
-    vv = vs(tt)
-
-    relsamp = np.arange(0, tt.shape[0], 1)
-
-    dd = pd.DataFrame({
-        "relsamp": relsamp,
-        "time": tt,
-        "x": xx,
-        "y": yy,
-        "v": vv
-    })
-
-    fig, ax = plt.subplots(2, 2, squeeze=False, figsize=(8, 8))
-    sns.scatterplot(data=d, x='x', y='y', hue='time', legend=None, ax=ax[0, 0])
-    sns.scatterplot(data=d,
-                    x='time',
-                    y='v',
-                    hue='time',
-                    legend=None,
-                    ax=ax[0, 1])
-    sns.scatterplot(data=dd,
-                    x='x',
-                    y='y',
-                    hue='time',
-                    legend=None,
-                    ax=ax[1, 0])
-    sns.scatterplot(data=dd,
-                    x='time',
-                    y='v',
-                    hue='time',
-                    legend=None,
-                    ax=ax[1, 1])
-    ax[0, 0].set_xlim(-11, 11)
-    ax[1, 0].set_xlim(-11, 11)
-    plt.tight_layout()
-    plt.show()
-
-
-d.groupby(["condition", "subject", "trial"]).apply(inspect_move)
-
-
-# d = d.groupby(["subject", "trial"], group_keys=False).apply(compute_vel)
-# d = d.groupby(["subject", "trial"], group_keys=False).apply(compute_mv)
-# d = d.sort_values(["condition", "subject", "trial"])
-
-# # TODO: interpoloate / smooth before or after?
-
-# fig, ax = plt.subplots(2, 2, squeeze=False)
-# [x.set_ylim(-20, 20) for x in ax.flatten()]
-# sns.lineplot(
-#     data=d[d["condition"] == 1],
-#     x="trial",
-#     y="imv",
-#     hue="sig_mp",
-#     style="phase",
-#     markers=True,
-#     legend=False,
-#     ax=ax[0, 0],
-# )
-# sns.lineplot(
-#     data=d[d["condition"] == 1],
-#     x="trial",
-#     y="emv",
-#     hue="sig_mp",
-#     style="phase",
-#     markers=True,
-#     legend=False,
-#     ax=ax[0, 1],
-# )
-# sns.lineplot(
-#     data=d[d["condition"] == 0],
-#     x="trial",
-#     y="imv",
-#     hue="sig_mp",
-#     style="phase",
-#     markers=True,
-#     legend=False,
-#     ax=ax[1, 0],
-# )
-# sns.lineplot(
-#     data=d[d["condition"] == 0],
-#     x="trial",
-#     y="emv",
-#     hue="sig_mp",
-#     style="phase",
-#     markers=True,
-#     legend=False,
-#     ax=ax[1, 1],
-# )
-# [x.set_xlabel("Trial") for x in ax.flatten()]
-# [x.set_ylabel("Initial Movement Vector") for x in ax[:, 0].flatten()]
-# [x.set_ylabel("Endpoint Movement Vector") for x in ax[:, 1].flatten()]
-# plt.tight_layout()
+# d.plot(subplots=True)
+# plt.show()
+# dd.plot(subplots=True)
+# plt.show()
+# ddd.plot(subplots=True)
 # plt.show()
 
+plot_mpep(d)
 
-# # dd = (
-# #     d.groupby(["subject", "phase", "trial", "sig_mp"])[["time", "x", "y", "v"]]
-# #     .apply(interpolate_movements)
-# #     .droplevel(-1)
-# #     .reset_index(drop=False)
-# # )
-
-# # dd = dd.sort_values(["trial", "relsamp"])
-
-# # dd.plot(subplots=True)
-# # plt.show()
-
-# # ddd = (
-# #     dd.groupby(["subject", "phase", "sig_mp", "relsamp"])[["time", "x", "y", "v"]]
-# #     .mean()
-# #     .reset_index()
-# # )
-
-# # ddd.plot(subplots=True)
-# # plt.show()
-
-# # fig, ax = plt.subplots(2, 2, squeeze=False)
-# # sns.scatterplot(
-# #     data=ddd[ddd["phase"] == "adapt_1"], x="x", y="y", hue="sig_mp", ax=ax[0, 0]
-# # )
-# # sns.scatterplot(
-# #     data=ddd[ddd["phase"] == "adapt_2"], x="x", y="y", hue="sig_mp", ax=ax[0, 1]
-# # )
-# # sns.scatterplot(
-# #     data=ddd[ddd["phase"] == "adapt_3"], x="x", y="y", hue="sig_mp", ax=ax[1, 0]
-# # )
-# # sns.scatterplot(
-# #     data=ddd[ddd["phase"] == "adapt_4"], x="x", y="y", hue="sig_mp", ax=ax[1, 1]
-# # )
-# # ax[0, 0].set_title("adapt 1")
-# # ax[0, 1].set_title("adapt 2")
-# # ax[1, 0].set_title("adapt 3")
-# # ax[1, 1].set_title("adapt 4")
-# # plt.tight_layout()
-# # plt.show()
-
+fig, ax = plt.subplots(2, 2, squeeze=False)
+sns.scatterplot(
+    data=ddd[ddd["phase"] == "adapt_1"], x="x", y="y", hue="sig_mp", ax=ax[0, 0]
+)
+sns.scatterplot(
+    data=ddd[ddd["phase"] == "adapt_2"], x="x", y="y", hue="sig_mp", ax=ax[0, 1]
+)
+sns.scatterplot(
+    data=ddd[ddd["phase"] == "adapt_3"], x="x", y="y", hue="sig_mp", ax=ax[1, 0]
+)
+sns.scatterplot(
+    data=ddd[ddd["phase"] == "adapt_4"], x="x", y="y", hue="sig_mp", ax=ax[1, 1]
+)
+ax[0, 0].set_title("adapt 1")
+ax[0, 1].set_title("adapt 2")
+ax[1, 0].set_title("adapt 3")
+ax[1, 1].set_title("adapt 4")
+plt.tight_layout()
+plt.show()
 
 # # dddd = dd.groupby(["subject", "relsamp"])[["time", "x", "y", "v"]].mean().reset_index()
 
--- vma_general/dhruva/code/util_func.py
+++ vma_general/dhruva/code/util_func.py
@@ -3,7 +3,7 @@ from imports import *
 
 def load_all_data():
     d_list = []
-    for sub_num in np.arange(1, 8, 1):
+    for sub_num in np.arange(1, 12, 1):
         if sub_num % 2 == 0:
             condition = 1
         else:
@@ -58,8 +58,6 @@ def load_all_data():
             ]
         ]
 
-        # d = d[np.isin(d["phase"], ["adapt_1", "adapt_2", "adapt_3", "adapt_4"])]
-
         d["sig_mp"] = (
             1 * d["no_uncertainty"]
             + 2 * d["low_uncertainty"]
@@ -75,31 +73,6 @@ def load_all_data():
 
     d = pd.concat(d_list)
 
-    return d
-
-
-def compute_vel(d):
-    t = d["time"].to_numpy()
-    x = d["x"].to_numpy()
-    y = d["y"].to_numpy()
-    vx = np.gradient(x, t)
-    vy = np.gradient(y, t)
-    v = np.sqrt(vx**2 + vy**2)
-    d["v"] = v
-    return d
-
-
-def compute_mv(d):
-    t = d["time"].to_numpy()
-    x = d["x"].to_numpy()
-    y = d["y"].to_numpy()
-    target_angle = d["target_angle"]
-    t = t - t.min()
-    theta, radius = cart2pol(x, y, units="deg")
-    imv = theta[radius < 5].mean() - target_angle
-    emv = theta[-1] - target_angle
-    d["imv"] = -imv
-    d["emv"] = -emv
     return d
 
 
@@ -121,5 +94,84 @@ def interpolate_movements(d):
     relsamp = np.arange(0, tt.shape[0], 1)
 
     dd = pd.DataFrame({"relsamp": relsamp, "time": tt, "x": xx, "y": yy, "v": vv})
+    dd["condition"] = d["condition"].unique()[0]
+    dd["subject"] = d["subject"].unique()[0]
+    dd["trial"] = d["trial"].unique()[0]
+    dd["phase"] = d["phase"].unique()[0]
+    dd["sig_mp"] = d["sig_mp"].unique()[0]
 
     return dd
+
+
+def compute_kinematics(d):
+    t = d["time"].to_numpy()
+    x = d["x"].to_numpy()
+    y = d["y"].to_numpy()
+
+    vx = np.gradient(x, t)
+    vy = np.gradient(y, t)
+    v = np.sqrt(vx**2 + vy**2)
+    d["v"] = v
+
+    v_peak = v.max()
+    ts = t[v > (0.05 * v_peak)][0]
+
+    radius = np.sqrt(x**2 + y**2)
+    theta = (np.arctan2(y, x)) * 180 / np.pi
+
+    imv = theta[(t >= ts) & (t <= ts + 0.1)].mean()
+    emv = theta[-1]
+
+    d["imv"] = imv
+    d["emv"] = emv
+
+    return d
+
+
+def plot_mpep(d):
+    fig, ax = plt.subplots(2, 2, squeeze=False)
+    sns.lineplot(
+        data=d[d["condition"] == 1],
+        x="trial",
+        y="imv",
+        hue="sig_mp",
+        style="phase",
+        markers=True,
+        legend=False,
+        ax=ax[0, 0],
+    )
+    sns.lineplot(
+        data=d[d["condition"] == 1],
+        x="trial",
+        y="emv",
+        hue="sig_mp",
+        style="phase",
+        markers=True,
+        legend=False,
+        ax=ax[0, 1],
+    )
+    sns.lineplot(
+        data=d[d["condition"] == 0],
+        x="trial",
+        y="imv",
+        hue="sig_mp",
+        style="phase",
+        markers=True,
+        legend=False,
+        ax=ax[1, 0],
+    )
+    sns.lineplot(
+        data=d[d["condition"] == 0],
+        x="trial",
+        y="emv",
+        hue="sig_mp",
+        style="phase",
+        markers=True,
+        legend=False,
+        ax=ax[1, 1],
+    )
+    [x.set_xlabel("Trial") for x in ax.flatten()]
+    [x.set_ylabel("Initial Movement Vector") for x in ax[:, 0].flatten()]
+    [x.set_ylabel("Endpoint Movement Vector") for x in ax[:, 1].flatten()]
+    plt.tight_layout()
+    plt.show()
